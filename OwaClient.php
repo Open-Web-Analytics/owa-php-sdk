4500fab58a0349a2beea5634c1c91b8301e74703
Peter Adams <peter@oncefuture.com>
2020-05-13 18:40:33 -0700

    adding support for obtaining credentials from env variable, constant, or credentials file.

--- OwaClient.php
+++ OwaClient.php
@@ -22,6 +22,70 @@ class OwaClient {
 		
 		$this->config[ $name ] = $value;
 	}
+	
+	public function getCredentials() {
+		
+		static $credentials;
+		
+		if ( empty ( $credentials ) ) {
+			
+			$credentials = [
+				
+				'api_key' => ''
+			];
+			
+			// check environment variables
+			$api_key = getenv('OWA_API_KEY');
+			
+			// check constant
+			if ($api_key ) {
+				
+				$credentials['api_key'] = $api_key;
+				
+			} else {
+				
+				if ( defined('OWA_API_KEY') ) {
+					
+					$credentials['api_key'] = OWA_API_KEY;
+				}
+				
+			}
+			
+			//check credentials file in home dir.
+			if (! $api_key ) {
+				
+				$file = $this->getHomeDir() . '/.owa/credentials';
+				
+				if ( file_exists( $file ) ){
+					
+					$file_contents = include( $file );
+					
+					if (is_array($file_contents) && array_key_exists( 'api_key', $file_contents ) ) {
+						
+						$credentials = $file_contents;
+					}
+				}
+			}
+						
+		} 
+		
+		return $credentials;
+	}
+	
+	private function getHomeDirectory() {
+        // On Unix systems, use the HOME environment variable
+        if ($home = getenv( 'HOME' ) ) {
+            
+            return $home;
+        }
+
+        // Get the HOMEDRIVE and HOMEPATH values for Windows hosts
+        $home_drive = getenv('HOMEDRIVE');
+        $home_path = getenv('HOMEPATH');
+
+        return ($home_drive && $home_path) ? $home_drive . $home_path : null;
+    }
+
 }	
 	
 ?>
